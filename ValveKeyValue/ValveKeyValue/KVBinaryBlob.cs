a157a151386bb5b96c8e0cce6879f6e0cf6f7991
Pavel Djundik <xPaw@users.noreply.github.com>
2022-12-02 21:42:11 +0200

    Implement binary blobs

---
+++ ValveKeyValue/ValveKeyValue/KVBinaryBlob.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+
+namespace ValveKeyValue
+{
+    public class KVBinaryBlob : KVValue
+    {
+        public byte[] Bytes { get; }
+
+        public override KVValueType ValueType => KVValueType.BinaryBlob;
+
+        public KVBinaryBlob(byte[] value)
+        {
+            Bytes = value;
+        }
+
+        #region IConvertible
+
+        public override TypeCode GetTypeCode()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool ToBoolean(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override byte ToByte(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override char ToChar(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override DateTime ToDateTime(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override decimal ToDecimal(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override double ToDouble(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override short ToInt16(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int ToInt32(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long ToInt64(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override sbyte ToSByte(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override float ToSingle(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override string ToString(IFormatProvider provider)
+             => ToString();
+
+        public override object ToType(Type conversionType, IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override ushort ToUInt16(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override uint ToUInt32(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override ulong ToUInt64(IFormatProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach (var oneByte in Bytes)
+            {
+                builder.Append(oneByte.ToString("X2"));
+                builder.Append(' ');
+            }
+
+            // Remove final space
+            if (builder.Length > 1)
+            {
+                builder.Length -= 1;
+            }
+
+            return builder.ToString();
+        }
+    }
+}
