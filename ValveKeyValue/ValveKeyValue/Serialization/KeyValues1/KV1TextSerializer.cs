ff6bbe7fe68c922f9a778160b88f09e7cbefca76
Pavel Djundik <xPaw@users.noreply.github.com>
2022-12-02 20:47:53 +0200

    Put commas after objects and arrays in arrays

--- ValveKeyValue/ValveKeyValue/Serialization/KeyValues1/KV1TextSerializer.cs
+++ ValveKeyValue/ValveKeyValue/Serialization/KeyValues1/KV1TextSerializer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Text;
-using System.Xml.Linq;
 using ValveKeyValue.Abstraction;
 
 namespace ValveKeyValue.Serialization.KeyValues1
